1cf507a312cb8e58a2fe7619deb9053d130bc02d
David Wingfield <wingfield4@marshall.edu>
2016-11-20 18:29:36 -0500

    Additions and changes to build from
    -added header (with navbar and necessary imports)
    -added main.css that is imported in header file
    -changed some names of codeIgnited files to fit better
    -added some example routes and views

---
+++ app/forum_project/views/forums.php
@@ -0,0 +1,30 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+include("header.html");
+?><!DOCTYPE html>
+<html lang="en">
+<head>
+  
+	<meta charset="utf-8">
+	<title>Forums</title>
+
+</head>
+<body>
+
+<div class="container">
+  <div class="row">
+    <div class="col-sm-8">
+      aksjdhf isbfiqo elid lgl aheo oid labeko oallle odqlnibg aksjdhf isbfiqo elid lgl aheo oid labeko oallle odqlnibg 
+      aksjdhf isbfiqo elid lgl aheo oid labeko oallle odqlnibg aksjdhf isbfiqo elid lgl aheo oid labeko oallle odqlnibg 
+      aksjdhf isbfiqo elid lgl aheo oid labeko oallle odqlnibg aksjdhf isbfiqo elid lgl aheo oid labeko oallle odqlnibg 
+      aksjdhf isbfiqo elid lgl aheo oid labeko oallle odqlnibg aksjdhf isbfiqo elid lgl aheo oid labeko oallle odqlnibg 
+    </div>
+    <div class="col-sm-4">
+      aksjdhf isbfiqo elid lgl aheo oid labeko oallle odqlnibg aksjdhf isbfiqo elid lgl aheo oid labeko oallle odqlnibg 
+      aksjdhf isbfiqo elid lgl aheo oid labeko oallle odqlnibg aksjdhf isbfiqo elid lgl aheo oid labeko oallle odqlnibg 
+      aksjdhf isbfiqo elid lgl aheo oid labeko oallle odqlnibg aksjdhf isbfiqo elid lgl aheo oid labeko oallle odqlnibg 
+      aksjdhf isbfiqo elid lgl aheo oid labeko oallle odqlnibg aksjdhf isbfiqo elid lgl aheo oid labeko oallle odqlnibg 
+  </div>
+</div>
+</body>
+</html>
