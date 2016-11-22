b4d663612055ebeea3026a5a2771b5948254f0db
David Wingfield <wingfield4@marshall.edu>
2016-11-22 16:54:53 -0500

    Altered base path
    Let me know if this ruins anything

--- app/forum_project/config/config.php
+++ app/forum_project/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = '/forum_project/app/index.php';
+$config['base_url'] = '/forum-project/app';
 
 /*
 |--------------------------------------------------------------------------
