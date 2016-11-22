802a2f038703645ffa904a5936bf35067b273f68
Mark Adkins <mark.adkins@sbcs.com>
2016-11-21 23:14:35 -0500

    started on home page

--- app/forum_project/views/home.php
+++ app/forum_project/views/home.php
@@ -9,19 +9,16 @@ include("header.html");
 </head>
 <body>
 
-<div id="container">
+<div class="container-fluid">
 	<h1>Welcome to CodeIgniter!</h1>
 
-	<div id="body">
-		<p>The page you are looking at is being generated dynamically by CodeIgniter.</p>
-
-		<p>If you would like to edit this page you'll find it located at:</p>
-		<code>application/views/welcome_message.php</code>
-
-		<p>The corresponding controller for this page is found at:</p>
-		<code>application/controllers/Welcome.php</code>
-
-		<p>If you are exploring CodeIgniter for the very first time, you should start by reading the <a href="user_guide/">User Guide</a>.</p>
+	<div class="row">
+		<div class="col-xs-12">
+			<div class="panel panel-primary">
+				<div class="panel-heading">Topic Title Here</div>
+				<div class="panel-body">Thread link here</div>
+			</div>
+		</div>
 	</div>
 
 	<p class="footer">Page rendered in <strong>{elapsed_time}</strong> seconds. <?php echo  (ENVIRONMENT === 'development') ?  'CodeIgniter Version <strong>' . CI_VERSION . '</strong>' : '' ?></p>
