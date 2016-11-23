7cfc24ae0e8b5f3f83acff2c2c320898e88928fc
Mark Adkins <mark.adkins@sbcs.com>
2016-11-23 15:28:34 -0500

    we'll figure these routes out eventually

--- app/forum_project/config/config.php
+++ app/forum_project/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = '/forum-project/app';
+$config['base_url'] = '/forum-project/app/index.php';
 
 /*
 |--------------------------------------------------------------------------
--- app/forum_project/config/routes.php
+++ app/forum_project/config/routes.php
@@ -49,11 +49,13 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | Examples:	my-controller/index	-> my_controller/index
 |		my-controller/my-method	-> my_controller/my_method
 */
-$route['default_controller'] = 'home';
+$route['default_controller'] = 'home/index';
 $route['404_override'] = '';
 $route['translate_uri_dashes'] = FALSE;
 
 //our routes
-$route['home'] = 'home';
+$route['home'] = 'home/index';
 $route['forum'] = 'forum';
 $route['user'] = 'user';
+
+$route['(:any)'] = 'home/index';
