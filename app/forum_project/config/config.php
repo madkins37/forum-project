ced63d80f48199ae52c96a17a09c2a36eb7da1d6
David Wingfield <wingfield4@marshall.edu>
2016-11-22 21:27:15 -0500

    small fix for redirect routes

--- app/forum_project/config/config.php
+++ app/forum_project/config/config.php
@@ -35,7 +35,7 @@ $config['base_url'] = '/forum-project/app';
 | variable so that it is blank.
 |
 */
-$config['index_page'] = 'index.php';
+$config['index_page'] = '';
 
 /*
 |--------------------------------------------------------------------------
