6290176db32a11d08bc992e1a01febacbea928a3
David Wingfield <wingfield4@marshall.edu>
2016-11-29 10:03:22 -0500

    started viewForm

---
+++ app/forum_project/views/viewForum.php
@@ -0,0 +1,27 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?><!DOCTYPE html>
+<html lang="en">
+<head>
+  
+	<meta charset="utf-8">
+	<title>Forums</title>
+
+</head>
+<body>
+<div class="container">
+	<div class="page-header">
+		<h1>Available Forums <small>For your reading pleasure</small></h1>
+	</div>
+  <div class="row">
+    <div class="col-sm-12">
+      <div id="forumsList">
+			</div>
+    </div>
+    <div class="col-sm-0">
+      
+    </div>
+  </div>
+	<?php include("../scripts/viewForumScript.php") ?>
+</body>
+</html>
